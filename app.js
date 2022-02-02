2a6335a27b782a7690f987643075cbb0cdc4cfb4
LucasSantamaria76 <luc_1976@hotmail.com>
2022-02-02 13:09:45 -0300

    terminando con el carrito

--- app.js
+++ app.js
@@ -1,32 +1,218 @@
-import { Data } from './data.js';
-import { ProductCard } from "./ProductCard.js";
+const $iconoUser = document.getElementById("iconoUser"),
+  $iconoLogin = document.getElementById("iconoLogin"),
+  $iconoCarrito = document.getElementById("iconoCarrito"),
+  $numItems = document.getElementById("num-items"),
+  $gridProductos = document.getElementById("grid-productos"),
+  $select = document.getElementById("select"),
+  $template = document.querySelector("#template-card").content,
+  $fragmento = document.createDocumentFragment(),
+  $carritoDesplegable = document.querySelector(".contenedor-carrito"),
+  $itemsCarrito = document.getElementById("itemsCarrito"),
+  $footerCarrito = document.getElementById("footerCarrito"),
+  $templateFooter = document.getElementById("template-footer").content,
+  $templateCarrito = document.getElementById("template-carrito").content;
 
-const $iconoUser = document.getElementById("iconoUser");
-const $iconoLogin = document.getElementById("iconoLogin");
+let productos, carrito, usuarioActivo, usuariosRegistrados;
 
-document.addEventListener("DOMContentLoaded", () => {
-  const usuario = JSON.parse(localStorage.getItem("user"));
-  if (usuario) {
-    $iconoLogin.style.display = "none";
-    $iconoUser.textContent = usuario.nombre[0];
-  }else $iconoUser.style.display = "none";
+const addCarrito = (e) => {
+  e.target.classList.contains("btn") && setCarrito(e.target.dataset.id);
+  e.stopPropagation();
+};
+
+const setCarrito = (id) => {
+  const nuevoProducto = {
+    nombre: productos[id].nombre,
+    precio: productos[id].precio,
+    id: id,
+    img: productos[id].img,
+    stock: productos[id].stock,
+    cantidad: 1,
+  };
+  if (carrito.hasOwnProperty(id)) {
+    nuevoProducto.cantidad = carrito[id].cantidad + 1;
+  }
+
+  let agregarCarrito = {};
+  agregarCarrito[id] = { ...nuevoProducto };
+  carrito = { ...carrito, ...agregarCarrito };
+  const nCantidad = Object.values(carrito).reduce(
+    (acc, { cantidad }) => acc + cantidad,
+    0
+  );
+  pintarCarrito();
+};
+
+const pintarCarrito = () => {
+  $itemsCarrito.innerHTML = "";
+  for (let key in carrito) {
+    $templateCarrito.querySelector("img").src = "./img/" + carrito[key].img;
+    $templateCarrito.querySelector("#item").textContent = carrito[key].nombre;
+    $templateCarrito.querySelector("input").value = carrito[key].cantidad;
+    $templateCarrito.querySelector("input").dataset.id = key;
+    $templateCarrito.querySelectorAll("span")[0].textContent =
+      carrito[key].precio;
+    $templateCarrito.querySelectorAll("span")[1].textContent =
+      carrito[key].precio * carrito[key].cantidad;
+
+    const $clone = $templateCarrito.cloneNode(true);
+    $fragmento.appendChild($clone);
+  }
+
+  $itemsCarrito.appendChild($fragmento);
+
+  usuariosRegistrados[usuarioActivo].carrito = carrito;
+  localStorage.setItem(
+    "UsuariosRegistrados",
+    JSON.stringify(usuariosRegistrados)
+  );
+
+  pintarFooter();
+};
+
+const pintarFooter = () => {
+  $footerCarrito.innerHTML = "";
+
+  if (Object.keys(carrito).length === 0) {
+    $footerCarrito.innerHTML = `
+    <h2 style="text-align:center;">Carrito vacío</h2>`;
+    $numItems.textContent = 0;
+    return;
+  }
+
+  // sumar cantidad y sumar totales
+
+  const nCantidad = Object.values(carrito).reduce(
+    (acc, { cantidad }) => acc + cantidad,
+    0
+  );
+
+  const nPrecio = Object.values(carrito).reduce(
+    (acc, { cantidad, precio }) => acc + cantidad * precio,
+    0
+  );
+
+  $templateFooter.querySelector(".nCantidad").textContent = nCantidad;
+  $templateFooter.querySelector("span").textContent = nPrecio;
+
+  const $clone = $templateFooter.cloneNode(true);
+  $fragmento.appendChild($clone);
+
+  $footerCarrito.appendChild($fragmento);
+  $numItems.textContent = nCantidad;
+
+  const vaciarCarrito = () => {
+    carrito = {};
+    $numItems.textContent = 0;
+    usuariosRegistrados[usuarioActivo].carrito = carrito;
+    localStorage.setItem(
+      "UsuariosRegistrados",
+      JSON.stringify(usuariosRegistrados)
+    );
+    pintarCarrito();
+  };
+
+  document
+    .querySelector(".btn-vaciar")
+    .addEventListener("click", () => vaciarCarrito());
+
+  document
+    .querySelector("#btnContinuar")
+    .addEventListener("click", () =>
+      $carritoDesplegable.classList.remove("mostarDesplegable")
+    );
+
+  document.querySelector("#btnComprar").addEventListener("click", () => {
+    vaciarCarrito();
+    $carritoDesplegable.classList.remove("mostarDesplegable");
+  });
+};
+
+const renderProductos = (tipoProducto = "Verduras") => {
+  fetch("./db.json")
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then((resProductos) => {
+      productos = resProductos[tipoProducto];
+      for (let key in productos) {
+        $template.querySelector("img").src = "./img/" + productos[key].img;
+        $template.querySelector("img").alt = productos[key].img;
+        $template.querySelector("H3").textContent = productos[key].nombre;
+        $template.querySelector("H4").textContent =
+          "precio: $ " + productos[key].precio;
+        $template.querySelector("button").dataset.id = key;
+        if(productos[key].stock == "0"){
+          $template.querySelector(".sinStock").style.display = "block";
+          $template.querySelector("button").disabled = true;
+        }else {
+          $template.querySelector("button").disabled = false;
+          $template.querySelector(".sinStock").style.display = "none";          
+        }
+
+        let clone = $template.cloneNode(true);
+        $fragmento.appendChild(clone);
+      }
+      $gridProductos.appendChild($fragmento);
+    })
+    .catch((error) => {
+      let msg = error.statusText || "Error al cargar los datos";
+      console.log(msg);
+      document.getElementById("grid-productos").innerHTML = `
+      <div class="error"><p>Error ${error.status}: ${msg}</p></div>`;
+    });
+};
+
+$select.addEventListener("change", (e) => {
+  $gridProductos.innerHTML = "";
+  renderProductos(e.target.value);
 });
 
-const $select = document.getElementById("select");
+document.addEventListener("DOMContentLoaded", () => {
+  usuariosRegistrados = JSON.parse(localStorage.getItem("UsuariosRegistrados"));
+  usuarioActivo = JSON.parse(localStorage.getItem("UsuarioActivo"));
 
-const renderProduct = (products) => {
-      const productos = $select.value === 'Frutas' ? products.Frutas : products.Verduras;
-      let html = "";
-      productos.forEach((el) => (html += ProductCard(el)));
-      document.getElementById("grid-productos").innerHTML = html;
-    }
+  if (usuarioActivo && usuariosRegistrados) {
+    $iconoLogin.style.display = "none";
+    $iconoUser.textContent = usuarioActivo[0];
+    $numItems.style.display = "block";
 
-  Data({
-    cbSuccess: (products) => renderProduct(products),
-  });
-  
-  $select.addEventListener("change", () => {
-    Data({
-      cbSuccess: (products) => renderProduct(products),
-    });
-  });
+    carrito = usuariosRegistrados[usuarioActivo].carrito;
+    const nCantidad = Object.values(carrito).reduce(
+      (acc, { cantidad }) => acc + cantidad,
+      0
+    );
+    $numItems.textContent = nCantidad;
+    pintarCarrito();
+  } else {
+    $numItems.style.display = "none";
+    $iconoUser.style.display = "none";
+  }
+  renderProductos();
+});
+
+$gridProductos.onclick = (e) => addCarrito(e);
+
+$iconoUser.onclick = () => {
+  if (confirm("¿Está seguro de cerrar la sesión?")) {
+    localStorage.removeItem("UsuarioActivo");
+    location.reload();
+  }
+};
+
+$iconoCarrito.onclick = (e) => {
+  e.preventDefault();
+  carrito && $carritoDesplegable.classList.toggle("mostarDesplegable");
+};
+
+$itemsCarrito.addEventListener("change", (e) => {
+  if (e.target.dataset.id) {
+    const producto = carrito[e.target.dataset.id];
+    producto.cantidad = Number(e.target.value);
+
+    producto.cantidad === 0
+      ? delete carrito[e.target.dataset.id]
+      : (carrito[e.target.dataset.id] = { ...producto });
+
+    pintarCarrito();
+  }
+
+  e.stopPropagation();
+});
--- login.js
+++ login.js
@@ -1,5 +1,3 @@
-import { Data } from "./data.js";
-
 const $userNombre = document.getElementById("userNombre");
 const $pass = document.getElementById("password");
 const $repetirPass = document.getElementById("RepetirPassword");
@@ -8,32 +6,35 @@ const $titulo = document.getElementById("titulo");
 const $btnEnviar = document.getElementById("btnEnviar");
 const $toast = document.getElementById("toast");
 
-
 // Modal
 const $showModal = document.querySelector(".modal");
 const $closeModal = document.querySelector(".modalClose");
 const $modalTitle = document.querySelector(".modalTitle");
 
-
-
 const msgDialog = (title) => {
   $modalTitle.textContent = title;
   $showModal.classList.add("modal-show");
-} 
+};
 
-$closeModal.addEventListener("click", (e) =>  $showModal.classList.remove("modal-show"));
+$closeModal.addEventListener("click", (e) =>
+  $showModal.classList.remove("modal-show")
+);
 
+let usuariosRegistrados = {};
 
 document.addEventListener("DOMContentLoaded", () => {
   $repetirPass.style.display = "none";
+  localStorage.getItem("UsuariosRegistrados") &&
+    (usuariosRegistrados = JSON.parse(
+      localStorage.getItem("UsuariosRegistrados")
+    ));
 });
-
 
 const showToast = (msg) => {
   $toast.textContent = msg;
   $toast.style.opacity = "1";
-  setTimeout(() => ($toast.style.opacity = "0"),1500);
-}
+  setTimeout(() => ($toast.style.opacity = "0"), 1500);
+};
 
 const loginRegistro = () => {
   if ($titulo.textContent === "Iniciar sesión") {
@@ -48,17 +49,16 @@ const loginRegistro = () => {
 };
 
 const enviarLoginRegistro = () => {
-  const loginUsuario = (usuarios) => {
-    const usuario = usuarios.filter((el) => el.nombre === $userNombre.value);
-    if (usuario.length) {
-      if (usuario[0].pass === $pass.value) {
+  const loginUsuario = () => {
+    if (usuariosRegistrados.hasOwnProperty($userNombre.value)) {
+      if (usuariosRegistrados[$userNombre.value].pass === $pass.value) {
         localStorage.setItem(
-          "user",
-          JSON.stringify({ id: usuario[0].id, nombre: usuario[0].nombre })
+          "UsuarioActivo",
+          JSON.stringify($userNombre.value)
         );
-        $userNombre.value = "";  
-        $pass.value = "";  
-        $repetirPass.value = "";  
+        $userNombre.value = "";
+        $pass.value = "";
+        $repetirPass.value = "";
         location.href = "index.html";
       } else {
         msgDialog("Password incorecto");
@@ -66,33 +66,84 @@ const enviarLoginRegistro = () => {
     } else {
       msgDialog("Usuario inexistente");
     }
+
+    /* const usuario = usuarios.filter((el) => el.nombre === $userNombre.value);
+    if (usuario.length) {
+      if (usuario[0].pass === $pass.value) {
+        localStorage.setItem(
+          "UsuarioActivo",
+          JSON.stringify(usuario[0].nombre)
+        );
+        $userNombre.value = "";
+        $pass.value = "";
+        $repetirPass.value = "";
+        location.href = "index.html";
+      } else {
+        msgDialog("Password incorecto");
+      }
+    } else {
+      msgDialog("Usuario inexistente");
+    } */
   };
-  
-  const registroUsuario = () => {};
-  
+
+  const registroUsuario = () => {
+    if ($pass.value === $repetirPass.value) {
+      const userId = Object.values(usuariosRegistrados).length
+        ? Object.values(usuariosRegistrados).length + 1
+        : 1;
+
+      const nuevoUsuario = {
+        id: userId,
+        pass: $pass.value,
+        carrito: [],
+      };
+
+      if (!usuariosRegistrados.hasOwnProperty($userNombre.value)) {
+        usuariosRegistrados[$userNombre.value] = { ...nuevoUsuario };
+        localStorage.removeItem("UsuariosRegistrados");
+        localStorage.setItem(
+          "UsuariosRegistrados",
+          JSON.stringify(usuariosRegistrados)
+        );
+        loginUsuario();
+      } else {
+        showToast("El usuario ingresado ya existe");
+        $userNombre.focus();
+      }
+    } else {
+      showToast("Las contraseñas no coinciden");
+      $repetirPass.focus();
+    }
+  };
+
   const isRegistro = $titulo.textContent === "Formulario de registro";
-  
+
   if (!$userNombre.value) {
-    showToast('Nombre de usuario no ingresado');
+    showToast("Nombre de usuario no ingresado");
     $userNombre.focus();
     return;
   }
-  
+
   if (!$pass.value) {
-    showToast('Password no ingresado');
+    showToast("Password no ingresado");
     $pass.focus();
     return;
   }
-  
+
   if (!$repetirPass.value && isRegistro) {
-    showToast('Debe repetir su password');
+    showToast("Debe repetir su password");
     $repetirPass.focus();
     return;
   }
-  
-  isRegistro
-    ? registroUsuario
-    : Data({ cbSuccess: (data) => loginUsuario(data.Users) });
+
+  if (!isRegistro) {
+    usuariosRegistrados = JSON.parse(
+      localStorage.getItem("UsuariosRegistrados")
+    );
+    usuariosRegistrados
+      ? loginUsuario()
+      : showToast("No hay usuarios registrados");
+  } else registroUsuario();
 };
 
 $enlaceReg.onclick = loginRegistro;
